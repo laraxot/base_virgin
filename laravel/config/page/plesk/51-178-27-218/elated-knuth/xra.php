cd632e04b5e69d47173fe2e5a135b30724714321
Ubuntu <ubuntu@vps-e25a78d0.vps.ovh.net>
2022-10-17 13:34:36 +0000

    up

---
+++ laravel/config/page/plesk/51-178-27-218/elated-knuth/xra.php
@@ -0,0 +1,13 @@
+<?php
+
+ return array (
+  'adm_theme' => 'AdminLTE',
+  'enable_ads' => '1',
+  'main_module' => 'IntelliNet',
+  'model' => 
+  array (
+    'profile' => 'Modules\IntelliNet\Models\Profile',
+  ),
+  'primary_lang' => 'it',
+  'pub_theme' => 'BsItalia',
+);
