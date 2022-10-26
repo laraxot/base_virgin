9e374ac21034c976fa9f09149e3613b577eac60f
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-26 17:22:50 +0000

    up

---
+++ laravel/config/page/plesk/51-178-27-218/elated-knuth/menu-item.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    0 => [
+        'id' => 2,
+        'label' => 'favorites',
+        'link' => '/it/favorites',
+        'menu' => 2,
+        'sort' => 1,
+        'parent' => null,
+        'class' => null,
+        'depth' => null,
+        'role_id' => null,
+        'logged_policy' => true,
+    ],
+    1 => [
+        'id' => 3,
+        'label' => 'scores',
+        'link' => '/it/pages/scores',
+        'menu' => 2,
+        'sort' => 2,
+        'parent' => null,
+        'class' => null,
+        'depth' => null,
+        'role_id' => null,
+        'logged_policy' => true,
+    ],
+    2 => [
+        'id' => 4,
+        'label' => 'alerts',
+
+        'link' => '/it/pages/alerts',
+        'menu' => 2,
+        'sort' => 3,
+        'parent' => null,
+        'class' => null,
+        'depth' => null,
+        'role_id' => null,
+        'logged_policy' => true,
+    ],
+    3 => [
+        'id' => 5,
+        'label' => 'clippings',
+        'link' => '/it/pages/clippings',
+        'menu' => 2,
+        'sort' => 4,
+        'parent' => null,
+        'class' => null,
+        'depth' => null,
+        'role_id' => null,
+        'logged_policy' => true,
+    ],
+];
---
+++ laravel/config/page/plesk/51-178-27-218/elated-knuth/menu_builder_item.php
@@ -0,0 +1,39 @@
+<?php
+                    return array (
+  0 => 
+  array (
+    'id' => 2,
+    'label' => 'Fonti',
+    'link' => '/it/reports',
+    'menu' => 2,
+    'sort' => 1,
+    'parent' => NULL,
+    'class' => NULL,
+    'depth' => NULL,
+    'role_id' => NULL,
+  ),
+  1 => 
+  array (
+    'id' => 3,
+    'label' => 'Keywords',
+    'link' => '/it/keywords',
+    'menu' => 2,
+    'sort' => 2,
+    'parent' => NULL,
+    'class' => NULL,
+    'depth' => NULL,
+    'role_id' => NULL,
+  ),
+  2 => 
+  array (
+    'id' => 4,
+    'label' => 'Plugins',
+    'link' => '/it/plugins',
+    'menu' => 2,
+    'sort' => 3,
+    'parent' => NULL,
+    'class' => NULL,
+    'depth' => NULL,
+    'role_id' => NULL,
+  ),
+);
